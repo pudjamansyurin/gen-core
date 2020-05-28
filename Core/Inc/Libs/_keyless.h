bf2293645b82c2660169f52fae3ec8ac74f39eef
geni <geni@gen-indonesia>
2020-05-28 12:28:06 +0700

    Added KLESS_Pairing Feature

--- Core/Inc/Libs/_keyless.h
+++ Core/Inc/Libs/_keyless.h
@@ -12,43 +12,40 @@
 #include "Drivers/_nrf24l01.h"
 
 /* Exported define -----------------------------------------------------------*/
-#define NRF_DATA_LENGTH											17										// Max: 32 bytes
-#define NRF_ADDR_LENGTH											5
-#define NRF_ADDRRESS_WIDTH							NRF_ADDR_WIDTH_5
+#define NRF_DATA_LENGTH											16U									// Max: 32 bytes
+#define NRF_ADDR_LENGTH                                          5U                                    // Range 3:5
 
 /* Exported enum -------------------------------------------------------------*/
 typedef enum {
-  KLESS_CMD_PING = 1,
-  KLESS_CMD_ALARM = 2,
-  KLESS_CMD_SEAT = 3,
-  KLESS_CMD_MAX = 3
+    KLESS_CMD_PING = 1,
+    KLESS_CMD_ALARM = 2,
+    KLESS_CMD_SEAT = 3,
+    KLESS_CMD_MAX = 3
 } KLESS_CMD;
 
 typedef enum {
-  KLESS_R = 0, KLESS_W = 1
+    KLESS_R = 0, KLESS_W = 1
 } KLESS_MODE;
 
 /* Exported struct -----------------------------------------------------------*/
 typedef struct {
-  uint8_t tx[NRF_ADDR_LENGTH];
-  uint8_t rx[NRF_ADDR_LENGTH];
-} kless_address_t;
-
-typedef struct {
-  uint8_t tx[NRF_DATA_LENGTH];
-  uint8_t rx[NRF_DATA_LENGTH];
-} kless_payload_t;
-
-typedef struct {
-  kless_address_t address;
-  kless_payload_t payload;
+    nrf24l01_config config;
+    struct {
+        uint8_t address[NRF_ADDR_LENGTH];
+        uint8_t payload[NRF_DATA_LENGTH];
+    } tx;
+    struct {
+        uint8_t address[NRF_ADDR_LENGTH];
+        uint8_t payload[NRF_DATA_LENGTH];
+    } rx;
 } kless_t;
 
 /* Public functions prototype ------------------------------------------------*/
 void KLESS_Init(void);
 void KLESS_Debugger(void);
 uint8_t KLESS_ValidateCommand(KLESS_CMD *cmd);
-uint8_t KLESS_UsePayload(KLESS_MODE mode, uint8_t *payload);
+uint8_t KLESS_Payload(KLESS_MODE mode, uint8_t *payload);
+uint8_t KLESS_Pairing(void);
 uint8_t KLESS_SendDummy(void);
 void KLESS_Refresh(void);
 void KLESS_IrqHandler(void);
--- Core/Src/Libs/_keyless.c
+++ Core/Src/Libs/_keyless.c
@@ -21,21 +21,19 @@ extern hmi1_t HMI1;
 
 /* Public variables -----------------------------------------------------------*/
 kless_t KLESS = {
-        .address = {
-                .tx = { 0, 0, 0, 0, 1 },
-                .rx = { 0, 0, 0, 0, 2 },
+        .config = {
+                .addr_width = NRF_ADDR_LENGTH - 2,
+                .payload_length = NRF_DATA_LENGTH,
+                .rx_buffer = NULL,
         },
-        .payload = {
-                .tx = { 0 },
-                .rx = { 0 },
-        }
-};
-
-/* Private variables ----------------------------------------------------------*/
-static nrf24l01_config config;
-static const kless_address_t KLESS_AddressDefault = {
-        .tx = { 0xAB, 0x00, 0x00, 0x00, 0x00 },
-        .rx = { 0xCD, 0x00, 0x00, 0x00, 0x00 },
+        .tx = {
+                .address = { 0xAB, 0x00, 0x00, 0x00, 0x00 },
+                .payload = { 0 },
+        },
+        .rx = {
+                .address = { 0xCD, 0x00, 0x00, 0x00, 0x00 },
+                .payload = { 0 },
+        },
 };
 
 /* Private functions declaration ---------------------------------------------*/
@@ -44,104 +42,144 @@ static void unlock(void);
 
 /* Public functions implementation --------------------------------------------*/
 void KLESS_Init(void) {
-	// set configuration
-	config.tx_address = KLESS_AddressDefault.tx;
-	config.rx_address = KLESS_AddressDefault.rx;
-	config.payload_length = NRF_DATA_LENGTH;
-	config.addr_width = NRF_ADDRRESS_WIDTH;
-	config.rx_buffer = (uint8_t*) KLESS.payload.rx;
-	nrf_set_config(&config);
+    // set configuration
+    KLESS.config.tx_address = (uint8_t*) KLESS.tx.address;
+    KLESS.config.rx_address = (uint8_t*) KLESS.rx.address;
+    KLESS.config.rx_buffer = (uint8_t*) KLESS.rx.payload;
+    nrf_set_config(&(KLESS.config));
 
-	// initialization
-	nrf_init(&nrf, &config);
-}
-
-void KLESS_Debugger(void) {
-	lock();
-	LOG_Str("NRF:Receive = ");
-	LOG_BufHex((char*) KLESS.payload.rx, NRF_DATA_LENGTH);
-	LOG_Enter();
-	unlock();
+    // initialisation
+    nrf_init(&nrf, &(KLESS.config));
 }
 
 uint8_t KLESS_ValidateCommand(KLESS_CMD *cmd) {
-	uint8_t pos, valid, payload[NRF_DATA_LENGTH] = { 0 };
+    uint8_t pos, valid, payload[NRF_DATA_LENGTH] = { 0 };
 
-	lock();
-	// Read Payload
-	valid = KLESS_UsePayload(KLESS_R, payload);
+    lock();
+    // Read Payload
+    valid = KLESS_Payload(KLESS_R, payload);
 
-	// Get Payload position
-	if (valid) {
-		pos = (payload[0] & 0xF0) >> 4;
-		*cmd = payload[pos] & 0x0F;
+    // Get Payload position
+    if (valid) {
+        pos = (payload[0] & 0xF0) >> 4;
+        *cmd = payload[pos] & 0x0F;
 
-		// Check Payload Command
-		if (*cmd > KLESS_CMD_MAX) {
-			valid = 0;
-		}
-	}
-	unlock();
+        // Check Payload Command
+        if (*cmd > KLESS_CMD_MAX) {
+            valid = 0;
+        }
+    }
+    unlock();
 
-	return valid;
+    return valid;
 }
 
-uint8_t KLESS_UsePayload(KLESS_MODE mode, uint8_t *payload) {
-	uint8_t ret = 0;
+uint8_t KLESS_Payload(KLESS_MODE mode, uint8_t *payload) {
+    uint8_t ret = 0;
 
-	lock();
-	// Process Payload
-	if (mode == KLESS_R) {
-		// Decrypt
-		if (AES_Decrypt(payload, KLESS.payload.rx, NRF_DATA_LENGTH)) {
-			ret = 1;
-		}
-	} else {
-		// Encrypt
-		if (AES_Encrypt(KLESS.payload.tx, payload, NRF_DATA_LENGTH)) {
-			ret = 1;
-		}
-	}
-	unlock();
+    lock();
+    // Process Payload
+    if (mode == KLESS_R) {
+        // Decrypt
+        if (AES_Decrypt(payload, KLESS.rx.payload, NRF_DATA_LENGTH)) {
+            ret = 1;
+        }
+    } else {
+        // Encrypt
+        if (AES_Encrypt(KLESS.tx.payload, payload, NRF_DATA_LENGTH)) {
+            ret = 1;
+        }
+    }
+    unlock();
 
-	return ret;
+    return ret;
+}
+
+uint8_t KLESS_Pairing(void) {
+    const uint8_t tx_address[NRF_ADDR_LENGTH] = { 0xAB, 0x00, 0x00, 0x00, 0x00 };
+    const uint8_t rx_address[NRF_ADDR_LENGTH] = { 0xCD, 0x00, 0x00, 0x00, 0x00 };
+    uint8_t payload[NRF_DATA_LENGTH + NRF_ADDR_LENGTH] = { 0 };
+    const uint8_t payload_length = NRF_DATA_LENGTH + NRF_ADDR_LENGTH;
+    uint32_t *pPayload;
+    NRF_RESULT p;
+
+    // Default pairing configuration
+    ce_reset(&nrf);
+    nrf_set_tx_address(&nrf, (uint8_t*) tx_address);
+    nrf_set_rx_address_p0(&nrf, (uint8_t*) rx_address);
+    nrf_set_rx_payload_width_p0(&nrf, payload_length);
+    ce_set(&nrf);
+
+    // Generate Payload
+    pPayload = (uint32_t*) payload;
+    // insert aes key
+    for (uint8_t i = 0; i < (NRF_DATA_LENGTH / sizeof(uint32_t)); i++) {
+        HAL_RNG_GenerateRandomNumber(&hrng, pPayload++);
+    }
+    // insert address
+    memcpy(&payload[NRF_DATA_LENGTH], KLESS.tx.address, NRF_ADDR_LENGTH);
+
+    // Send Payload
+    p = nrf_send_packet(&nrf, payload);
+
+    // Retrieve back the configuration
+    ce_reset(&nrf);
+    nrf_set_tx_address(&nrf, nrf.config.tx_address);
+    nrf_set_rx_address_p0(&nrf, nrf.config.rx_address);
+    nrf_set_rx_payload_width_p0(&nrf, nrf.config.payload_length);
+    ce_set(&nrf);
+
+    // debug
+    LOG_Str("NRF:Send = ");
+    LOG_Str(p ? "OK" : "ERROR");
+    LOG_Enter();
+
+    return (p == NRF_OK);
 }
 
 uint8_t KLESS_SendDummy(void) {
-	uint8_t payload[NRF_DATA_LENGTH] = { 0 };
-	NRF_RESULT p;
+    uint8_t payload[NRF_DATA_LENGTH] = { 0 };
+    NRF_RESULT p;
 
-	payload[0] = 1;
-	p = nrf_send_packet(&nrf, payload);
+    payload[0] = 1;
+    p = nrf_send_packet(&nrf, payload);
 
-	// debug
-	LOG_Str("NRF:Send = ");
-	LOG_Str(p ? "OK" : "ERROR");
-	LOG_Enter();
+    // debug
+    LOG_Str("NRF:Send = ");
+    LOG_Str(p ? "OK" : "ERROR");
+    LOG_Enter();
 
-	return (p == NRF_OK);
+    return (p == NRF_OK);
+}
+
+void KLESS_Debugger(void) {
+    lock();
+    LOG_Str("NRF:Receive = ");
+    LOG_BufHex((char*) KLESS.rx.payload, NRF_DATA_LENGTH);
+    LOG_Enter();
+    unlock();
 }
 
 void KLESS_Refresh(void) {
-	if ((osKernelGetTickCount() - VCU.d.tick.keyless)
-	        < pdMS_TO_TICKS(KEYLESS_TIMEOUT)) {
-		HMI1.d.status.keyless = 1;
-	} else {
-		HMI1.d.status.keyless = 0;
-	}
+    if ((osKernelGetTickCount() - VCU.d.tick.keyless)
+            < pdMS_TO_TICKS(KEYLESS_TIMEOUT)) {
+        HMI1.d.status.keyless = 1;
+    } else {
+        HMI1.d.status.keyless = 0;
+    }
 }
 
 void KLESS_IrqHandler(void) {
-	nrf_irq_handler(&nrf);
-	osThreadFlagsSet(KeylessTaskHandle, EVT_KEYLESS_RX_IT);
+    nrf_irq_handler(&nrf);
+    osThreadFlagsSet(KeylessTaskHandle, EVT_KEYLESS_RX_IT);
 }
 
 /* Private functions implementation --------------------------------------------*/
 static void lock(void) {
-	osMutexAcquire(KlessRecMutexHandle, osWaitForever);
+    osMutexAcquire(KlessRecMutexHandle, osWaitForever);
 }
 
 static void unlock(void) {
-	osMutexRelease(KlessRecMutexHandle);
+    osMutexRelease(KlessRecMutexHandle);
 }
 
