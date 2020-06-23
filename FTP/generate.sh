7fbd1ebe365378784bd5c4439a2e0f3a1ae0b824
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-06-23 15:42:17 +0700

    Added jacksum.

--- FTP/generate.sh
+++ FTP/generate.sh
@@ -6,6 +6,6 @@ APP_CRC_FILE="APP.crc"
 
 cp $SRC_BIN_FILE $APP_BIN_FILE
 #objcopy -I binary -O binary --reverse-bytes=4 $SRC_BIN_FILE "tmp.bin"
-java -jar ~/jacksum.jar -a crc32_mpeg2 -X -F "#CHECKSUM" -O $APP_CRC_FILE $APP_BIN_FILE
+java -jar ./jacksum.jar -a crc32_mpeg2 -X -F "#CHECKSUM" -O $APP_CRC_FILE $APP_BIN_FILE
 
-echo "Done."
+echo "Done, ready to upload."
