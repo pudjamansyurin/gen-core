ce0c6d221b1586be9ce7e4d9c8619da1714f42b4
Pudja Mansyurin <pujakusumae@hotmail.com>
2020-11-12 15:37:56 +0700

    Increase the firmware version
    Signed-off-by: Pudja Mansyurin <pujakusumae@hotmail.com>

--- VCU-APP/Core/Inc/_defines.h
+++ VCU-APP/Core/Inc/_defines.h
@@ -14,6 +14,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "_defines_shared.h"
 
-#define VCU_VERSION                  (uint16_t) 304
+#define VCU_VERSION                  (uint16_t) 305
 
 #endif /* DEFINES_H_ */
