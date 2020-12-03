5bdb0f1de4312d2ca9c35421ba8c2edf96232daf
Puja Kusuma <pujakusumae@hotmail.com>
2020-12-03 11:16:13 +0700

    Increment VCU vesion

--- VCU-APP/Core/Inc/_defines.h
+++ VCU-APP/Core/Inc/_defines.h
@@ -14,6 +14,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "_defines_shared.h"
 
-#define VCU_VERSION                  (uint16_t) 320
+#define VCU_VERSION                  (uint16_t) 321
 
 #endif /* DEFINES_H_ */
