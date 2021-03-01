3d243aa2bb5324d3ec2b782ba41000ad1ff983d8
Puja Kusuma <pujakusumae@hotmail.com>
2021-03-01 15:21:13 +0700

    Upgrade version

--- VCU-APP/Core/Inc/_defines.h
+++ VCU-APP/Core/Inc/_defines.h
@@ -14,6 +14,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "_defines_shared.h"
 
-#define VCU_VERSION                  (uint16_t) 582
+#define VCU_VERSION                  (uint16_t) 583
 
 #endif /* DEFINES_H_ */
