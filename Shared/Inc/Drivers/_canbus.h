e1aae2b40014f8e59b6ed21f4f48332ede91e430
Puja Kusuma <pujakusumae@hotmail.com>
2021-03-16 17:19:03 +0700

    Added MCU can protocol

--- Shared/Inc/Drivers/_canbus.h
+++ Shared/Inc/Drivers/_canbus.h
@@ -11,19 +11,20 @@
 /* Includes ------------------------------------------------------------------*/
 #include "Libs/_utils.h"
 
+#define CAN_DATA 	        UNION64
 /* Exported defines ----------------------------------------------------------*/
 typedef enum {
-	CAND_FOCAN_PROGRESS 		= 0x101,
-	CAND_FOCAN_CRC 					= 0x102,
-	CAND_FOCAN_PRA 					= 0x103,
-	CAND_FOCAN_INIT 				= 0x104,
-	CAND_FOCAN_RUNNING 			= 0x105,
-	CAND_FOCAN_PASCA 				= 0x106,
+	CAND_FOCAN_PROGRESS 		= 0x031,
+	CAND_FOCAN_CRC 					= 0x032,
+	CAND_FOCAN_PRA 					= 0x033,
+	CAND_FOCAN_INIT 				= 0x034,
+	CAND_FOCAN_RUNNING 			= 0x035,
+	CAND_FOCAN_PASCA 				= 0x036,
 
-	CAND_VCU_SWITCH 				= 0x200,
-	CAND_VCU_DATETIME 			= 0x201,
-	CAND_VCU_SELECT_SET 		= 0x202,
-	CAND_VCU_TRIP_MODE 			= 0x203,
+	CAND_VCU_SWITCH 				= 0x050,
+	CAND_VCU_DATETIME 			= 0x051,
+	CAND_VCU_SELECT_SET 		= 0x052,
+	CAND_VCU_TRIP_MODE 			= 0x053,
 
 	CAND_HMI1 							= 0x7C0,
 	CAND_HMI2 							= 0x7D0,
@@ -31,19 +32,34 @@ typedef enum {
 	CAND_BMS_SETTING 				= 0x000001B2,
 	CAND_BMS_PARAM_1 				= 0x0B0FFFFF,
 	CAND_BMS_PARAM_2 				= 0x0B1FFFFF,
+
 	CAND_BMS_TO_CHARGER 		= 0x0E0FFFFF,
 	CAND_BMS_CODE 					= 0x0E1FFFFF,
 	CAND_BMS_FROM_CHARGER 	= 0x0E3FFFFF,
-
 	CAND_BMS_CELL_1 				= 0x0B4,
 	CAND_BMS_CELL_2 				= 0x0B5,
 	CAND_BMS_CELL_3 				= 0x0B6,
 	CAND_BMS_CELL_4 				= 0x0B7,
-	CAND_BMS_RATED 					= 0x0BA
+	CAND_BMS_RATED 					= 0x0BA,
 
+	CAND_MCU_MESSAGE				= 0x0C0,
+	CAND_MCU_RW_PARAM       = 0x0C1,
+
+	CAND_MCU_MODULE_TEMP   	= 0x0A0,
+	CAND_MCU_TORQUE_SPEED  	= 0x0A1,
+	CAND_MCU_MODULE_PHASE  	= 0x0A2,
+	CAND_MCU_CURRENT      	= 0x0A3,
+	CAND_MCU_FAULT_CODE   	= 0x0A4,
+	CAND_MCU_MODULATION    	= 0x0A5,
+	CAND_MCU_STATES					= 0x0A6,
+
+	CAND_MCU_ERROR_1				= 0x0A7,
+	CAND_MCU_ERROR_2				= 0x0A8,
+	CAND_MCU_ERROR_3				= 0x0AB,
+	CAND_MCU_ERROR_4				= 0x0AC,
+	CAND_MCU_ERROR_5				= 0x0AD,
+	CAND_MCU_ERROR_6				= 0x0AF,
 } CAN_ADDR;
-
-#define CAN_DATA 	                   UNION64
 
 /* Exported struct ------------------------------------------------------------*/
 typedef struct {
